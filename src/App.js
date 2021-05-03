63204add496055b59aed5f912fa5900df063575d
khaos <khaos991213@gmail.com>
2021-05-03 20:24:40 +0800

    'hw3'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Signin from './SingIn';
+import Pic from './AddPic';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {Signin() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
